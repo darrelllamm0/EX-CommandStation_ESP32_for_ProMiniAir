da55bf7ed367672ec1a8c0a6e1b6d403d13c7f18
Darrell Lamm <darrelllamm0@gmail.com>
2024-02-20 18:27:45 -0500

    New.

---
+++ CommandStation-EX/DRLdefines.h
@@ -0,0 +1,31 @@
+// DRL: Begin
+#ifndef DRL_DEFINES_H
+#define DRL_DEFINES_H
+
+#undef INSERT_IDLE
+#define FAKE_ACK
+
+#undef RESET_PREAMBLE_COUNT
+#define ADD_IDLE_PACKET
+// This may be a problem
+#define ADD_MSGS
+#define ADD_MSGS_EPS32
+
+#define ADD_DRIVER
+#define ADD_OLED
+
+#define ADD_JOIN
+#define INSERT_NMRADCC
+
+#if defined(INSERT_NMRADCC)
+#define MSG_MAX 16
+#endif
+
+// Does not work to my
+// satisfaction
+#undef FORGETLOCO
+
+#undef DEBUG
+
+#endif
+// DRL: End
---
+++ CommandStation-EX/mySetup.h
@@ -0,0 +1,8 @@
+#include "DRLDefines.h"
+// DRL: Begin
+#if defined(ADD_JOIN)
+// Output Main on Programming Track output, Pin 13 based on my version of config.h
+#pragma message "Adding <1 JOIN> to startup"
+SETUP("<1 JOIN>");
+#endif
+// DRL: End
